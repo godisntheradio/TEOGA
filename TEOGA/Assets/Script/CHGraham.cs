46d5a74198cc55f1f0b1e2cdd9990178a13aea1b
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2021-04-26 12:19:28 -0300

    Linhas dos angulos

---
+++ TEOGA/Assets/Script/CHGraham.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace ConvexHull
+{
+
+    class Point
+    {
+        public Vector3 position;
+
+        public float angle;
+
+        public GameObject objectRef;
+
+        public Point(Vector3 p, GameObject objectRef)
+        {
+            this.position = p;
+            this.angle = PolarAngle(p);
+            this.objectRef = objectRef;
+        }
+
+        public float PolarAngle(Vector3 p)
+        {
+            return Mathf.Atan(p.y / p.x);
+        }
+    }
+
+
+    class CHGraham
+    {
+        private List<GameObject> ObjectList;
+        public GameObject SmallestY;
+        private List<Point> Points;
+        public List<GameObject> Hull;
+
+        public CHGraham(List<GameObject> points)
+        {
+            ObjectList = points;
+            Hull = new List<GameObject>();
+            Points = new List<Point>();
+        }
+
+        public void FindSmallestY()
+        {
+            SmallestY = ObjectList.OrderBy((i) => i.transform.position.y).FirstOrDefault();
+        }
+
+        public void CalculateConvexHull()
+        {
+            FindSmallestY();
+
+            ObjectList.Remove(SmallestY);
+
+            foreach (var item in ObjectList)
+            {
+                Points.Add(new Point(item.transform.position - SmallestY.transform.position, item));
+            }
+
+            Points = Points.OrderBy(p => p.angle < 0.0f).ThenBy(p => p.angle).ToList();
+        }
+
+        public List<Vector3> GetVectorList()
+        {
+            var list = new List<Vector3>();
+            list.Add(SmallestY.transform.position);
+            foreach (var item in Points)
+            {
+                list.Add(item.position);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ TEOGA/Assets/Script/PathBuilder.cs
@@ -0,0 +1,78 @@
+﻿using ConvexHull;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathBuilder : MonoBehaviour
+{
+    public GameObject PointsContainer;
+
+
+    List<GameObject> Points;
+
+    CHGraham Graham;
+
+
+    #region Visual
+
+    public Material AngleLinesMaterial;
+    public Material LineHullMaterial;
+    public Material LinePathMaterial;
+
+    private Mesh AngleLines;
+    private Matrix4x4 AngleLinesMatrix;
+
+    #endregion
+
+    void Start()
+    {
+        Points = new List<GameObject>();
+        foreach (Transform item in PointsContainer.transform)
+        {
+            Points.Add(item.gameObject);
+        }
+        Graham = new CHGraham(Points);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AngleLines != null)
+        {
+            Graphics.DrawMesh(AngleLines, AngleLinesMatrix, AngleLinesMaterial, 0);
+        }
+    }
+
+    public void BeginProcessing()
+    {
+        Graham.CalculateConvexHull();
+
+        Mesh lines = new Mesh();
+        var points = Graham.GetVectorList();
+
+        Vector3[] vertices = new Vector3[points.Count];
+        Vector2[] uv = new Vector2[points.Count];
+        int[] indices = new int[points.Count * 2];
+
+        int j = 0;
+        for (int i = 1; i < points.Count; i++)
+        {
+            vertices[i] = points[i];
+            uv[i] = points[i];
+            indices[j] = 0;
+            indices[j + 1] = i;
+            j += 2;
+        }
+
+        lines.SetVertices(vertices);
+        lines.uv = uv;
+        lines.SetIndices(indices, MeshTopology.Lines, 0);
+
+        AngleLines = lines;
+        AngleLinesMatrix = Matrix4x4.TRS(Graham.SmallestY.transform.position, Quaternion.identity, Vector3.one);
+        AngleLinesMatrix = PointsContainer.transform.worldToLocalMatrix * AngleLinesMatrix;
+    }
+
+    
+
+}
